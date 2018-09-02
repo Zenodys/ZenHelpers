789c2332cb43b5e3cd74c22a84d7e5dcee95fba4
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2018-09-02 22:37:45 +0200

    Initial commit for UI helper

--- VisualWrappers/Computing/main.js
+++ VisualWrappers/Computing/main.js
@@ -1,6 +1,13 @@
-const {app, BrowserWindow} = require('electron');
-const { spawn } = require('child_process');
-const {ipcMain} = require('electron');
+const {
+  app,
+  BrowserWindow
+} = require('electron');
+const {
+  spawn
+} = require('child_process');
+const {
+  ipcMain
+} = require('electron');
 const path = require('path');
 
 let mainWindow;
@@ -8,44 +15,49 @@ let child;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-      height: 600,
-      width: 800
+    height: 600,
+    width: 800
   });
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
 
-ipcMain.on("showVisualisations",function (event, arg) {
-    var newWindow        = new BrowserWindow({ width: 450, height: 300, show: 
-                                          false,webPreferences: {webSecurity: false,plugins:
-                                          true,nodeIntegration: false} });
-    newWindow.loadURL("http://localhost:9999/home/index");
-    newWindow.show();
+ipcMain.on("showVisualisations", function (event, arg) {
+  var newWindow = new BrowserWindow({
+    width: 450,
+    height: 300,
+    show: false,
+    webPreferences: {
+      webSecurity: false,
+      plugins: true,
+      nodeIntegration: false
+    }
+  });
+  newWindow.loadURL("http://localhost:9999/home/index");
+  newWindow.show();
 });
 
-ipcMain.on("startEngine",function (event, arg) {
-    child = spawn(path.join(__dirname, "ZenEngine\\ZenEngine.exe"),
-      {
-        cwd: path.join(__dirname, "ZenEngine")
-      }
-    );
+ipcMain.on("startEngine", function (event, arg) {
+  child = spawn(path.join(__dirname, "ZenEngine\\ZenEngine.exe"), {
+    cwd: path.join(__dirname, "ZenEngine")
+  });
 
-		child.stdout.on('data', (data) => {
-			console.log(data.toString());
-      mainWindow.webContents.send('onZenEngineOutput' , data.toString());
-		});
+  child.stdout.on('data', (data) => {
+    console.log(data.toString());
+    mainWindow.webContents.send('onZenEngineOutput', data.toString());
+  });
 
-		child.stderr.on('data', (data) => {
-      var arg = data != null ? data : '';
-      mainWindow.webContents.send("onZenEngineError", arg);
-		});
-		
-    child.on('exit',(code, signal)=>{
-      mainWindow.webContents.send("onZenEngineExit");
-    });
+  child.stderr.on('data', (data) => {
+    var arg = data != null ? data : '';
+    mainWindow.webContents.send("onZenEngineError", arg);
+  });
+
+  child.on('exit', (code, signal) => {
+    mainWindow.webContents.send("onZenEngineExit");
+  });
 });
 
-ipcMain.on("stopEngine",function (event, arg) {
+ipcMain.on("stopEngine", function (event, arg) {
   child.stdin.pause();
-	child.kill();
+  child.kill();
 });
