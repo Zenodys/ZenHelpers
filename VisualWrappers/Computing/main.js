107f3cd4764af85ee797faee4c981382b07a3022
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2018-09-05 02:17:56 +0200

    Uptime time duration attached to engine Added app icon

--- VisualWrappers/Computing/main.js
+++ VisualWrappers/Computing/main.js
@@ -1,6 +1,13 @@
-const {app, BrowserWindow} = require('electron');
-const { spawn } = require('child_process');
-const {ipcMain} = require('electron');
+const {
+  app,
+  BrowserWindow
+} = require('electron');
+const {
+  spawn
+} = require('child_process');
+const {
+  ipcMain
+} = require('electron');
 const path = require('path');
 
 let mainWindow;
@@ -8,15 +15,16 @@ let child;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-      height: 600,
-      width: 800
+    height: 1024,
+    width: 1280,
+    icon: path.join(__dirname, 'assets/icons/zenodys-icon-128x128.ico')
   });
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     mainWindow = null;
   });
-  
+
 });
 
 app.on('window-all-closed', () => {
@@ -24,60 +32,59 @@ app.on('window-all-closed', () => {
     app.quit();
 });
 
-ipcMain.on("showVisualisations",function (event, arg) {
-    var newWindow        = new BrowserWindow({ width: 450, height: 300, show: 
-                                          false,webPreferences: {webSecurity: false,plugins:
-                                          true,nodeIntegration: false} });
-    newWindow.loadURL("http://localhost:9999/home/index");
-    newWindow.show();
+ipcMain.on("showVisualisations", function (event, arg) {
+  var newWindow = new BrowserWindow({
+    width: 450,
+    height: 300,
+    show: false,
+    webPreferences: {
+      webSecurity: false,
+      plugins: true,
+      nodeIntegration: false
+    }
+  });
+  newWindow.loadURL("http://localhost:9999/home/index");
+  newWindow.show();
 });
 
-ipcMain.on("startEngine",function (event, arg) {
-    child = spawn(path.join(__dirname, "ZenEngine\\ZenEngine.exe"),
-      {
-        cwd: path.join(__dirname, "ZenEngine")
-      }
-    );
+ipcMain.on("startEngine", function (event, arg) {
+  child = spawn(path.join(__dirname, "ZenEngine\\ZenEngine.exe"), {
+    cwd: path.join(__dirname, "ZenEngine")
+  });
 
-		child.stdout.on('data', (data) => {
-      if (isJSON(data.toString())) {
-        var cnt = JSON.parse(data);
-        sendToRenderer ('onZenEngineCount' , cnt.Data);    
-      }
-      else{
-        console.log(data.toString());
-        sendToRenderer('onZenEngineOutput' , data.toString());  
-      }
-    });
+  child.stdout.on('data', (data) => {
+    if (isJSON(data.toString())) {
+      var cnt = JSON.parse(data);
+      sendToRenderer('onZenEngineCount', cnt.Data);
+    } else {
+      console.log(data.toString());
+      sendToRenderer('onZenEngineOutput', data.toString());
+    }
+  });
 
-		child.stderr.on('data', (data) => {
-      var arg = data != null ? data : '';
-      sendToRenderer("onZenEngineError", arg);
-		});
-		
-    child.on('exit',(code, signal)=>{
-      sendToRenderer("onZenEngineExit");
-    });
+  child.stderr.on('data', (data) => {
+    var arg = data != null ? data : '';
+    sendToRenderer("onZenEngineError", arg);
+  });
+
+  child.on('exit', (code, signal) => {
+    sendToRenderer("onZenEngineExit");
+  });
 });
 
-ipcMain.on("stopEngine",function (event, arg) {
+ipcMain.on("stopEngine", function (event, arg) {
   child.stdin.pause();
-	child.kill();
+  child.kill();
 });
 
-function sendToRenderer(topic, arg)
-{
+function sendToRenderer(topic, arg) {
   if (mainWindow != null && mainWindow.webContents != null)
     mainWindow.webContents.send(topic, arg);
 }
 
-function isJSON(text)
-{
-  if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').
-    replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
-    replace(/(?:^|:|,)(?:\s*\[)+/g, '')))
+function isJSON(text) {
+  if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, '')))
     return true
   else
     return false;
 }
-
