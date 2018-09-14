a02db63c542fbd3f1edba28ef8b01b18bc4f88c7
Tomaž Vinko <tomaz@zenodys.com>
2018-09-14 21:30:03 +0200

    syntactic bug fixes

--- VisualWrappers/Computing/main.js
+++ VisualWrappers/Computing/main.js
@@ -86,13 +86,13 @@ ipcMain.on("stopEngine", function(event, arg) {
 });
 
 function sendToRenderer(topic, arg) {
-    if (mainWindow != null && mainWindow.webContents != null)
+    if (mainWindow !== null && mainWindow.webContents !== null)
         mainWindow.webContents.send(topic, arg);
 }
 
 function isJSON(text) {
     if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, '')))
-        return true
+        return true;
     else
         return false;
 }
